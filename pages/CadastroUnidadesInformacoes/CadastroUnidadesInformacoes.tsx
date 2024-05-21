a1468c92f9220b08a20f968de6cf688923dc6df7
Luan <118026093+Luanfelima@users.noreply.github.com>
2024-05-21 03:58:30 +0000

    21/05/24 Tela de Cadastro Unidade funcionando

--- pages/CadastroUnidadesInformacoes/CadastroUnidadesInformacoes.tsx
+++ pages/CadastroUnidadesInformacoes/CadastroUnidadesInformacoes.tsx
@@ -8,33 +8,56 @@ export function ButtonCantinas() {
     const campos = document.querySelectorAll<HTMLInputElement>('input[type="text"]');
     campos.forEach((campo) => {
       campo.value = '';
+      campo.classList.remove(styles.erro); // Remove a classe de erro ao limpar os campos
     });
   };
-  
-  return (
-  <Layout>
-    <div className={styles.container}> {/* Adiciona a classe container */}
-      <div className={styles.input}> {/*Adiciona a classe input */}
-        <div className={styles.ColunaEsquerda}> {/*Adiciona div para coluna esquerda*/}
-          <input type='text' id='texto' placeholder='Nome Unidade'></input>
-          <input type='text' id='texto' placeholder='CEP'></input>
-          <input type='text' id='texto' placeholder='Cidade'></input>
-          <input type='text' id='texto' placeholder='Número'></input>
-        </div>
-          <div className={styles.ColunaDireita}> {/*Adiciona div para coluna direita*/}
-            <input type='text' id='texto' placeholder='CNPJ'></input>
-            <input type='text' id='texto' placeholder='Endereço Unidade'></input>
-            <input type='text' id='texto' placeholder='Estado'></input>
-            <input type='text' id='texto' placeholder='Complemento'></input>
-          </div>
-      </div>
 
-          <div className={styles.buttonContainer}> {/* Container para os botões */}
-            <Button>Cadastrar</Button>
-            <Button onClick={limparCampos}>Limpar</Button>
+  const verificarCampos = () => {
+    const campos = document.querySelectorAll<HTMLInputElement>('input[type="text"]');
+    let todosPreenchidos = true;
+
+    campos.forEach((campo) => {
+      if (campo.id !== 'complemento' && campo.value.trim() === '') {
+        campo.classList.add(styles.erro); // Adiciona uma classe de erro se o campo estiver vazio
+        todosPreenchidos = false;
+      } else {
+        campo.classList.remove(styles.erro); // Remove a classe de erro se o campo estiver preenchido
+      }
+    });
+
+    if (todosPreenchidos) {
+      alert('Unidade Cadastrada com Sucesso!');
+      limparCampos();
+    }
+    else {
+      alert('Por favor, preencha todos os campos obrigatórios.');
+    }
+  };
+
+  return (
+    <Layout>
+      <div className={styles.container}> {/* Adiciona a classe container */}
+        <div className={styles.input}> {/*Adiciona a classe input */}
+          <div className={styles.ColunaEsquerda}> {/*Adiciona div para coluna esquerda*/}
+            <input type='text' id='nomeUnidade' placeholder='*Nome Unidade'></input>
+            <input type='text' id='cep' placeholder='*CEP'></input>
+            <input type='text' id='cidade' placeholder='*Cidade'></input>
+            <input type='text' id='numero' placeholder='*Número'></input>
           </div>
-    </div>
-  </Layout>
+          <div className={styles.ColunaDireita}> {/*Adiciona div para coluna direita*/}
+            <input type='text' id='cnpj' placeholder='*CNPJ'></input>
+            <input type='text' id='enderecoUnidade' placeholder='*Endereço Unidade'></input>
+            <input type='text' id='estado' placeholder='*Estado'></input>
+            <input type='text' id='complemento' placeholder='Complemento'></input>
+          </div>
+        </div>
+
+        <div className={styles.buttonContainer}> {/* Container para os botões */}
+          <Button onClick={verificarCampos}>Cadastrar</Button>
+          <Button onClick={limparCampos}>Limpar</Button>
+        </div>
+      </div>
+    </Layout>
   );
 }
 
