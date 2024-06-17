92d747e0cb2779ce4c8b98cb5991c53c27203ce1
Luan <118026093+Luanfelima@users.noreply.github.com>
2024-06-17 23:26:45 +0000

    17/06 Versão 1.1 CantinaSmart

--- componentes/NavbarSimpleColored.tsx
+++ componentes/NavbarSimpleColored.tsx
@@ -43,7 +43,7 @@ export function NavbarSimpleColored() {
         <Group className={classes.header} justify="space-between">
           <span>CantinaSmart</span>
           <Code fw={700} className={classes.version}>
-            v1.0
+            v1.1
           </Code>
         </Group>
         {links}
